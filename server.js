2c12a980dee2f474d5bf0d8ed74ead0256c3013a
Mahmoud <b00037739@aus.edu>
2019-12-27 10:20:47 +0400

    Server Running, and shows post number in the terminal

--- server.js
+++ server.js
@@ -1,11 +1,43 @@
-var path = require('path')
-var mangoose = require('mangoose')
-
+//create new express and save it in app
 var express = require('express');
 var app = express ();
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error'));
-db.once('open', function(){
-	console.log("Connected to database")
+//Server configuration
+const PORT = 8080;
+
+
+app.get("/", function(req ,res){
+	Subject.find(function(error,silence){
+		if (error)
+			return console.error(error);
+		res.send(silence);
+	});
 });
+
+app.get("/hello", function(req ,res){
+	res.send("Sup dude")
+});
+
+app.post("/", function(req ,res){
+	res.send("Sup dude")
+});
+
+//Create app listener 
+app.listen(PORT, () =>{
+	console.log('Server running at: http://localhost:${PORT}');
+});
+
+
+app.get('/',(req, res) => {
+	res.send('Hello World');
+});
+
+
+
+
+// // //checks if the server is connected to the database and retuens a message
+// // var db = mongoose.connection;
+// // db.on('error', console.error.bind(console, 'connection error'));
+// // db.once('open', function(){
+// // 	console.log("Connected to database")
+// // });
